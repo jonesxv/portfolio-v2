dcc56c251fb206c55f7e8e08125474f015a49e3a
Ryan Jones <ryanjones33@gmail.com>
2019-06-24 18:16:56 -0700

    Animations

--- public/javascripts/animations.js
+++ public/javascripts/animations.js
@@ -1,6 +1,9 @@
 $(window).on("scroll", function(e){
   var proj = $('#projects').offset().top
+  var opac = 1 - ($(window).scrollTop() / proj)
+  $('.intro').css({'background-color': `rgba(40, 45, 68, ${opac})`})
   if ($(window).scrollTop() > proj) {
+    
     $('.nav').fadeIn('fast', function() {
       // Animation complete
     });
@@ -10,3 +13,40 @@ $(window).on("scroll", function(e){
     });
   }
 })
+
+var scroll = window.requestAnimationFrame ||
+  function(callback) {
+    window.setTimeout(callback
+      , 1000/60)}
+var show = document.querySelectorAll('.scroll-animate');
+console.log(show)
+function loop() {
+  show.forEach(el => {
+    if (isElementInViewport(el)) {
+      el.classList.add('visible')
+    } else {
+      el.classList.remove('visible')
+    }
+  })
+  scroll(loop)
+}
+loop();
+
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
+  }
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0
+      && rect.bottom >= 0)
+    ||
+    (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+    ||
+    (rect.top >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+  );
+}
